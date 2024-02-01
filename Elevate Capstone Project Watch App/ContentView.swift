d8281b18be998974b942ca602de363cb3a1f0392
Paul Kenniston <kennistonpaul@gmail.com>
2024-01-31 17:40:43 -0800

    Initial Commit

---
+++ Elevate Capstone Project Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elevate Capstone Project Watch App
+//
+//  Created by Kripa Sitaraman on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("test, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elevate Capstone Project Watch App/Elevate_Capstone_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elevate_Capstone_ProjectApp.swift
+//  Elevate Capstone Project Watch App
+//
+//  Created by Kripa Sitaraman on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Elevate_Capstone_Project_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
